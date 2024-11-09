ad4f515a392b6f08c45b8c6cbd3315b568b2c289
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2024-11-09 16:13:31 +0200

    Update hacking.py

--- hacking.py
+++ hacking.py
@@ -1,1 +1,30 @@
+import sys
 
+pass_list = ('RESOLVE', 'CHICKEN', 'ADDRESS', 'DISPLAY', 'PENALTY', 'REFUGEE', 'IMPROVE')
+attempts = 5
+
+def hack(pass_list, attempts):
+
+	if attempts == 0:
+		print('Sorry, ciao')
+		sys.exit()
+
+	variety = input('Enter a password:\n')
+
+	if variety not in pass_list:
+	    print('NOT close')
+	    attempts -= 1
+	    hack(pass_list, attempts)
+
+	elif variety in pass_list and variety != pass_list[-1]:
+	    right_pass = 'IMPROVE'
+	    count = sum(1 for i in range(min(len(variety), len(right_pass))) if variety[i] == right_pass[i])
+	    print(f"Matches: {count}/7")
+	    attempts -= 1
+	    hack(pass_list, attempts)
+
+	else:
+	    print('Password is IMPROVE')
+
+if __name__ == '__main__':
+	hack(pass_list, attempts)
